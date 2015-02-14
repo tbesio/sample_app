687744fb15d8fad550460a5d72c368ab1ade1629
Tom Besio <thomas.besio@gmail.com>
2015-02-14 18:47:00 +0000

    Complete through listing 6.40

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255}, format: { with: VALID_EMAIL_REGEX }
   
   has_secure_password
+  validates :password, length: { minimum: 6}
 end
